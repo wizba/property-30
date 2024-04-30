db716e9b85527b4f5b6c2655016129c272ef1261
william <williamrabs@gmail.com>
2024-05-01 00:06:12 +0200

    test

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const test = () => {
-  return <div>test</div>;
+const PropertyAddPage = () => {
+  return <div>PropertyAddPage</div>;
 };
 
-export default test;
+export default PropertyAddPage;
