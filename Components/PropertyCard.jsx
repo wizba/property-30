317b3dd749417a22fb513a9ec2d3324e3ca59e68
william <williamrabs@gmail.com>
2024-04-30 02:50:43 +0200

    Removing .env

--- Components/PropertyCard.jsx
+++ Components/PropertyCard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaBed, FaBath, FaRulerCombined, FaMoneyBill, FaMapMarker } from 'react-icons/fa' 
+import {
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaMoneyBill,
+  FaMapMarker,
+} from "react-icons/fa";
 
 const PropertyCard = ({ property }) => {
   const getRateDisplay = () => {
@@ -28,8 +34,8 @@ const PropertyCard = ({ property }) => {
       />
       <div className="p-4">
         <div className="text-left md:text-center lg:text-left mb-6">
-          <div className="text-gray-600">Apartment</div>
-          <h3 className="text-xl font-bold">Boston Commons Retreat</h3>
+          <div className="text-gray-600">{property.type}</div>
+          <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
         <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
           ${getRateDisplay()}
@@ -37,15 +43,15 @@ const PropertyCard = ({ property }) => {
 
         <div className="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
-            <FaBed className="inline mr-2"/> {property.beds}{" "}
+            <FaBed className="inline mr-2" /> {property.beds}{" "}
             <span className="md:hidden lg:inline">Beds</span>
           </p>
           <p>
-          <FaBath className="inline mr-2"/>  {property.baths}{" "}
+            <FaBath className="inline mr-2" /> {property.baths}{" "}
             <span className="md:hidden lg:inline">Baths</span>
           </p>
           <p>
-            <FaRulerCombined className="inline mr-2"/> 
+            <FaRulerCombined className="inline mr-2" />
             {property.square_feet}{" "}
             <span className="md:hidden lg:inline">sqft</span>
           </p>
@@ -54,19 +60,19 @@ const PropertyCard = ({ property }) => {
         <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
           {property.rates.nightly && (
             <p>
-               <FaMoneyBill className="inline mr-2"/>  Nightly
+              <FaMoneyBill className="inline mr-2" /> Nightly
             </p>
           )}
 
           {property.rates.weekly && (
             <p>
-              <FaMoneyBill className="inline mr-2"/> Weekly
+              <FaMoneyBill className="inline mr-2" /> Weekly
             </p>
           )}
 
           {property.rates.monthly && (
             <p>
-              <FaMoneyBill className="inline mr-2"/> Monthly
+              <FaMoneyBill className="inline mr-2" /> Monthly
             </p>
           )}
         </div>
@@ -75,8 +81,11 @@ const PropertyCard = ({ property }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="flex align-middle gap-2 mb-4 lg:mb-0">
-          <FaMapMarker className="text-orange-700 mt-1"/>
-            <span className="text-orange-700"> {property.location.city} {property.location.state} </span>
+            <FaMapMarker className="text-orange-700 mt-1" />
+            <span className="text-orange-700">
+              {" "}
+              {property.location.city} {property.location.state}{" "}
+            </span>
           </div>
           <Link
             href={`/properties/${property._id}`}
